50f002f34905f89729b25134c4723e7eec9126b0
alison.andrade <alison.andrade@academico.ifpb.edu.br>
2024-03-13 11:39:51 -0300

    atualizando2

--- 1048.py
+++ 1048.py
@@ -1,13 +1,13 @@
 salario = float(input())
 percentual = 0
 
-if 0 == salario <= 400.00:
+if salario <= 400.00:
     percentual = 15
-elif 400.01 == salario <= 800.00:
+elif salario <= 800.00:
     percentual = 12
-elif 800.01 == salario <= 1200.00:
+elif salario <= 1200.00:
     percentual = 10
-elif 1200.01 == salario <= 2000.001:
+elif salario <= 2000.001:
     percentual = 7
 elif salario > 2000.00:
     percentual = 4
@@ -15,6 +15,6 @@ elif salario > 2000.00:
 reajuste = salario * (percentual / 100)
 novo_salario = salario + reajuste
 
-print(f'{novo_salario:.2f}')
-print(f'{reajuste:.2f}')
-print(f'{percentual}')
+print(f'Novo salario: {novo_salario:.2f}')
+print(f'Reajuste ganho: {reajuste:.2f}')
+print(f'Em percentual: {percentual} %')
